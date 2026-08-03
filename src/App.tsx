253b98b57a70b6d7b6a8d51a80d81e4683260fa6
samukelo <sbkhathi005@gmail.com>
2026-08-03 15:54:38 +0200

    Testing Deployment

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div className='bg-blue w-80 h-200 flex column'>
-        <h1 className='text-3xl color-blue'>Hello world</h1>
+        <h1 className='text-3xl color-blue'>Testing Deployment</h1>
       </div>
     </>
   )
