1adeb32b58401ce2f09980533effc1b1cc1a7047
samukelo <sbkhathi005@gmail.com>
2026-08-11 09:39:58 +0200

    Fixing the CLI issue

---
+++ src/Pages/CurrentWeather.tsx
@@ -0,0 +1,18 @@
+// import react from "react";
+// import { useParams } from "react-router-dom";
+// // import Card from "../Components/Ui/Card/Card";
+
+
+// const CurrentWeather = () => {
+//     const { location, } = useParams();
+//     return (
+//         <div>
+//             <Card>
+//                 <h1>Current Weather for {location}</h1>
+//             </Card>
+
+//         </div>
+//     );
+// };
+
+// export default CurrentWeather;
