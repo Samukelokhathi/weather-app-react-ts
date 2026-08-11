c18838c055ec244fbfbea5c7ea1c72ceebe800b0
samukelo <sbkhathi005@gmail.com>
2026-08-11 04:12:35 +0200

    Added saveLocation and removeLocation functions in storage.ts

--- src/utils/Storage.ts
+++ src/utils/Storage.ts
@@ -4,3 +4,17 @@ export function getSavedLocations(): string[] {
     const savedLocations = localStorage.getItem(storageKey);
     return savedLocations ? JSON.parse(savedLocations) : [];
 }
+
+export function saveLocation(location: string): string[] {
+    const currentData = getSavedLocations();
+    if (currentData.includes(location)) return currentData;
+    const updated = [...currentData, location];
+    localStorage.setItem(storageKey, JSON.stringify(updated));
+    return updated;
+}
+
+export function removeLocation(city: string): string[] {
+    const updated = getSavedLocations().filter((c) => c !== city);
+    localStorage.setItem(storageKey, JSON.stringify(updated));
+    return updated;
+}
