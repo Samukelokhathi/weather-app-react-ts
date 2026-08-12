a6004ffb577b428e5e56398598507bf44ae3e769
samukelo <sbkhathi005@gmail.com>
2026-08-12 13:33:19 +0200

    Added daily slider in forecast.tsx

--- src/Components/Forecast/Forecast.tsx
+++ src/Components/Forecast/Forecast.tsx
@@ -12,18 +12,29 @@ interface ForecastProps {
 
 const Forecast: React.FC<ForecastProps> = ({ weather }) => {
     const [view, setView] = useState<"hourly" | "daily">("hourly");
+
     return (
-        <Card className="flex flex-col gap-4">
-            <div className="flex justify-between items-center mb-4">
-                <Text variant="h3" children="5-Day Forecast" />
+        <Card className="flex flex-col gap-4 bg-[#122033] p-4">
+            <Text variant="h3" children="5-Day Forecast" />
 
-                <div className="flex gap-2 bg-white/5 rounded-full p-1">
-                    <Button className="bg-white/10 hover:bg-white/20" onClick={() => setView("hourly")} text="Hourly"
-                    />
-                    <Button className="bg-white/10 hover:bg-white/20" onClick={() => setView("daily")} text="Daily"
-                    />
+            <div className="flex gap-2 bg-white/5 rounded-full p-1">
+                <Button className="bg-white/10 hover:bg-white/20" onClick={() => setView("hourly")} text="Hourly"
+                />
 
-                </div>
+                <Button className="bg-white/10 hover:bg-white/20" onClick={() => setView("daily")} text="Daily"
+                />
+            </div>
+
+
+            <div className="flex gap-3 overflow-x-auto pb-2">
+                {view === "daily" && weather.days.map((day) => (
+                    <Card key={day.datetime} className="">
+                        <Text variant="h4" children={day.datetime} />
+                        <span>{getWeatherIcon(day.icon)}</span>
+                        <Text variant="p" children={`${day.temp}°C`} />
+
+                    </Card>
+                ))}
             </div>
 
         </Card>
