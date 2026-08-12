8621a58da29270e6f340a69d9c69e92fb0a66b9f
samukelo <sbkhathi005@gmail.com>
2026-08-12 14:09:22 +0200

    Added hourly slider with card

--- src/Components/Forecast/Forecast.tsx
+++ src/Components/Forecast/Forecast.tsx
@@ -15,31 +15,44 @@ const Forecast: React.FC<ForecastProps> = ({ weather }) => {
 
     return (
         <Card className="flex flex-col gap-4">
-            <div className="flex justify-between items-center mb-4">
-                <Text variant="h3" children="Forecast" />
 
-                <div className="flex gap-2 bg-white/5 rounded-full p-1">
-                    <Button className={`px-4 py-1 rounded-full text-sm ${view === "hourly" ? "bg-[#20B5E2] text-white" : "text-gray-400"
+            <div className="flex gap-2 justify-between  p-1">
+                <Text variant="h3" children="Forecast" />
+                <div>
+                    <Button className={`px-4 py-1 rounded-0  text-sm ${view === "hourly" ? "bg-[#20B5E2] text-white" : "text-gray-400"
                         }`} onClick={() => setView("hourly")} text="Hourly"
                     />
-
-                    <Button className={`px-4 py-1 rounded-full text-sm ${view === "daily" ? "bg-[#20B5E2] text-white" : "text-gray-400"
+                    <Button className={`px-4 py-1  text-sm ${view === "daily" ? "bg-[#20B5E2] text-white" : "text-gray-400"
                         }`} onClick={() => setView("daily")} text="Daily"
                     />
                 </div>
-
-
-                <div className="flex gap-3 overflow-x-auto pb-2 snap-x snap-mandatory">
-                    {view === "daily" && weather.days.map((day) => (
-
-                        <Card key={day.datetime} className="flex-col items-center gap-2 px-4 py-4 min-w-[90px] snap-start shrink-0 bg-white/5">
-                            <Text variant="h4" children={day.datetime} />
-                            <span>{getWeatherIcon(day.icon)}</span>
-                            <Text variant="p" children={`${day.temp}°C`} />
-                        </Card>
-                    ))}
-                </div>
             </div>
+
+
+            <div className="flex gap-3 overflow-x-auto pb-2 ">
+                {view === "daily" && weather.days.map((day) => (
+
+                    <Card key={day.datetime} className="flex-col items-center gap-2 px-4 py-4 min-w-[90px] shrink-0 bg-white/5">
+
+                        <Text variant="h4" children={day.datetime} />
+                        <span>{getWeatherIcon(day.icon)}</span>
+                        <Text variant="p" children={`${day.temp}°C`} />
+
+                    </Card>
+                ))}
+
+                {view === "hourly" && weather.days[0].hours.map((hour) => (
+
+                    <Card key={hour.datetime} className="flex-col items-center gap-2 px-4 py-4 min-w-[90px] shrink-0 bg-white/5">
+
+                        <Text variant="h4" children={hour.datetime} />
+                        <span>{getWeatherIcon(hour.icon)}</span>
+                        <Text variant="p" children={`${hour.temp}°C`} />
+                    </Card>
+                ))}
+            </div>
+
+
 
         </Card>
     );
